9f58108ffe02eb64eaa31678bd10c9bf64885575
Tom Sullivan <tom@msbit.com.au>
2021-04-08 17:06:06 +1000

    Remove default `binary` argument for `hash`

--- src/create_function.php
+++ src/create_function.php
@@ -6,7 +6,7 @@ function create_function(string $args, string $code): string {
   static $salt = null;
   $salt ??= bin2hex(random_bytes(64));
 
-  $hash = hash('sha512', "({$args}) {{$code}} | {$salt}", false);
+  $hash = hash('sha512', "({$args}) {{$code}} | {$salt}");
   $name = "lambda_{$hash}";
 
   if (!function_exists($name)) {
